10bc24a9a79b85298b4368606700e7053b028537
nanki <1113049408@qq.com>
2026-08-06 20:36:01 +0800

    chore(miniapp): add release readiness gate

--- aisoftoj-mini/config/prod.ts
+++ aisoftoj-mini/config/prod.ts
@@ -1,5 +1,6 @@
 import type { UserConfigExport } from '@tarojs/cli'
 
 export default {
+  projectConfigName: process.env.TARO_APP_PROJECT_CONFIG || 'project.config.json',
   mini: {}
 } satisfies UserConfigExport
